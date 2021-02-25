54acd5b3688ef8073331b4c676bffd88deca58d9
@Dam74523 <militar74523@wp.pl>
2021-02-25 14:50:03 +0100

    Dodanie metod obsługujacej klase sqlSklep

---
+++ src/main/java/SqlKraj.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class SqlKraj {
+    private int kodKraju;
+    private String nazwaKraju;
+
+    Scanner scanner = new Scanner(System.in);
+
+    private void scannNazwaKraju(){
+        System.out.println("Podaj nazwe kraju");
+        nazwaKraju = scanner.next();
+    }
+
+    private void scannKodKraju(String choice){
+        System.out.println("Podaj kod kraju");
+        if(choice.equals("delete")){
+            System.out.println("któru chcesz usunąć");
+        }
+        kodKraju = scanner.nextInt();
+    }
+
+    public String addKraj(){
+        scannKodKraju("add");
+        scannNazwaKraju();
+
+        return "INSERT INTO asortyment " + "VALUES ("+kodKraju+","+nazwaKraju+")";
+
+    }
+
+    public String deleteKraj(){
+        scannKodKraju("delete");
+
+        return "Delete from kraj where nazwa ="+kodKraju;
+    }
+
+}
